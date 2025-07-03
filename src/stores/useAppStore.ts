8b0d2651ad5e9fb8939037df914d42a3816c4b8b
DaniM0613 <wwh.rj.92@gmail.com>
2025-07-02 22:05:56 -0600

    Creanod el slice de AI
    En nuestra carpeta store, creamos un nuevo archivo llamado aiSlice.ts.
    Dentro de este archivo, definimos un tipo llamado AISlice, que contiene una propiedad recipe de tipo string.
    Luego, creamos una función llamada createAISlice, la cual está tipada con StateCreator de la librería zustand.
    A esta función le pasamos el genérico correspondiente a nuestro tipo AISlice, y finalmente, devolvemos un objeto que contiene el estado inicial del slice, donde recipe es un string vacío

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -3,10 +3,12 @@ import { createRecipeSlice, type RecipeSliceType } from "./recipeSlice";
 import { devtools } from "zustand/middleware";
 import { createFavoritesSlice, type FavoritesSliceType } from "./favoriteSlice";
 import { createNotificationSlice, type NotificatioSliceType } from "./notificationSlice";
+import { createAISlice, type AISlice } from "./aiSlice";
 
 
-export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificatioSliceType>()(devtools((...a) => ({
+export const useAppStore = create<RecipeSliceType & FavoritesSliceType & NotificatioSliceType & AISlice>()(devtools((...a) => ({
    ...createRecipeSlice(...a),
    ...createFavoritesSlice(...a),
-   ...createNotificationSlice(...a)
+   ...createNotificationSlice(...a),
+   ...createAISlice(...a)
 })))
