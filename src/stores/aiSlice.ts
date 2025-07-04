49a9ac438cd4dbb5309e2ef442130387b7cea647
DaniM0613 <wwh.rj.92@gmail.com>
2025-07-04 15:24:57 -0600

    Comunicando el componente con el slice

--- src/stores/aiSlice.ts
+++ src/stores/aiSlice.ts
@@ -3,8 +3,12 @@ import type { StateCreator } from "zustand"
 
 export type AISlice = {
     recipe: string
+    generateRecipe: (promp: string) => Promise<void>
 }
 
 export const createAISlice: StateCreator<AISlice, [], [], AISlice> = () => ({
-    recipe: ''
+    recipe: '',
+    generateRecipe: async(prompt) => {
+        console.log(prompt)
+    }
 })
